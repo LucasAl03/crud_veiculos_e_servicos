422574ed42bde0706b763d5de1d1704dfdc0081e
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-07 20:23:10 -0300

    Definidas as rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Formulario } from './component/formulario/formulario';
+import { HomeComponent } from './component/home-component/home-component';
+import { Lista } from './component/lista/lista';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'formulario',
+        component: Formulario
+    },
+    {
+        path: 'lista',
+        component: Lista
+    }
+];
