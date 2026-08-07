dfebb4bab47f92857c1f079b3981e04927cdfa22
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-07 20:59:59 -0300

    Adicionado os atributos do formulario

--- src/app/component/formulario/formulario.ts
+++ src/app/component/formulario/formulario.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulario.html',
   styleUrl: './formulario.css',
 })
-export class Formulario {}
+export class Formulario {
+  marca_veiculo : string = ''
+  modelo_veiculo : string = ''
+  placa_veiculo : string = ''
+  cambio_veiculo : string = ''
+  movido_veiculo : string = ''
+  servico_veiculo : string = ''
+  valor_servico : number = 0.0
+}
