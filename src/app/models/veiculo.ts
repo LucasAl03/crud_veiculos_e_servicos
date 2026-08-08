16cbfc1864411b80d9af39722c6b644ae734b530
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-07 21:20:24 -0300

    classe veiculo atualizada

--- src/app/models/veiculo.ts
+++ src/app/models/veiculo.ts
@@ -1,7 +1,10 @@
 export class Veiculo{
     id?: number
+    marcaVeiculo?: string
     modeloVeiculo?: string
     placaVeiculo?: string
+    cambioVeiculo?: string
     movidoVeiculo?: string
-    
+    servicoVeiculo?: string
+    valorVeiculo?: number
 }
