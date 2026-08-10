c59769da654bca30847293682b9902cef925e766
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-10 13:34:48 -0300

    service funções listar, adicionar, tamanhoArray, buscaId, editar e excluir

--- src/app/services/veiculos-servico-service.ts
+++ src/app/services/veiculos-servico-service.ts
@@ -1,4 +1,40 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Veiculo } from '../models/veiculo';
+import { of } from 'rxjs';
 
-@Service()
-export class VeiculosServicoService {}
+@Injectable({
+    providedIn: 'root',
+})
+export class VeiculosServicoService {
+    private veiculos : Veiculo [] = [];
+
+    adicionar(pessoa: Veiculo){
+        this.veiculos.push(pessoa)
+    }
+
+    listar(): Veiculo[]{
+        return this.veiculos
+    }
+
+    tamanhoArray(): number{
+        return this.veiculos.length;
+    }
+
+    buscaId(id: number){
+        const veiculo = this.veiculos.find(elem => elem.id == id);
+
+        return of(veiculo);
+    }
+
+    editar(veiculo: Veiculo){
+        const posArray = this.veiculos.findIndex(elem => elem.id == veiculo.id);
+
+        if (posArray !== -1){
+            this.veiculos[posArray] = veiculo;
+        }
+    }
+
+    excluir(id: number){
+        this.veiculos = this.veiculos.filter(elem => elem.id !== id);
+    }
+}
