b5b1f0d2f02e78016f8a99c2ef227e04c5165cd0
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-10 13:33:15 -0300

    função listar, exluir e buscaId feitas para a lista

--- src/app/component/lista/lista.ts
+++ src/app/component/lista/lista.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, numberAttribute } from '@angular/core';
+
+import { VeiculosServicoService } from '../../services/veiculos-servico-service';
+
+import { Veiculo } from '../../models/veiculo';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lista',
@@ -6,4 +12,26 @@ import { Component } from '@angular/core';
   templateUrl: './lista.html',
   styleUrl: './lista.css',
 })
-export class Lista {}
+export class Lista {
+
+  constructor(
+    private router: Router,
+    private veiculosServicoService: VeiculosServicoService
+  ){}
+
+  listar(){
+    return this.veiculosServicoService.listar();
+  }
+
+  excluir(vObjVeiculo: Veiculo){
+    if(confirm("Deseja excluir as informações deste veículo?")){
+      this.veiculosServicoService.excluir(Number(vObjVeiculo.id));
+    }
+
+    this.listar();
+  }
+
+  buscaId(vObjVeiculo: Veiculo){
+    this.router.navigate(['/formulario', vObjVeiculo.id]);
+  }
+}
